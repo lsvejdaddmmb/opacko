b2be61e3a6afb93b1f042c9bba57c6e2a2b3923a
Libor Svejda <libor.svejda@gmail.com>
2020-05-26 21:16:27 +0200

    zapamatovani jmena na serveru

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 let mujCitac = 0;
+let jmeno = "???";
 
 function main(req, res) {
     console.log("url: " + req.url);
-    if (req.url === "/") {
+    let q = url.parse(req.url, true);
+    console.log("cesta: " + q.pathname);
+    console.log("parametry: " + JSON.stringify(q.query));
+    if (q.pathname === "/") {
         let s = fs.readFileSync("index.html");
         res.writeHead(200, {"Content-type": "text/html"});
         res.end(s);
-    } else if (req.url === "/citac") {
+    } else if (q.pathname === "/citac") {
         mujCitac++; //dtto mujCitac = mujCitac + 1;
         let obj = {};
         obj.citac = mujCitac;
         res.writeHead(200, {"Content-type": "application/json"});
         res.end(JSON.stringify(obj));
-    } else if (req.url === "/citac/vynuluj") {
+    } else if (q.pathname === "/citac/vynuluj") {
         mujCitac = 0;
         let obj = {};
         obj.citac = mujCitac;
+        res.writeHead(200, {"Content-type": "application/json"});
+        res.end(JSON.stringify(obj));
+    } else if (q.pathname === "/zapamatujsime") {
+        jmeno = q.query.jmeno;
+        let obj = {};
+        obj.status = "zapamatovano!";
+        res.writeHead(200, {"Content-type": "application/json"});
+        res.end(JSON.stringify(obj));
+    } else if (q.pathname === "/kohosipamatujes") {
+        let obj = {};
+        obj.zapamatovanejmeno = jmeno;
         res.writeHead(200, {"Content-type": "application/json"});
         res.end(JSON.stringify(obj));
     } else {
